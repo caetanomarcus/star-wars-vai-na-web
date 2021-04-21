7e687959efd93eb6ff3bc69072c647fdf65ada2a
Marcus <caetanomarcusf@gmail.com>
2021-04-20 21:55:45 -0300

    hover 105% igual ao design

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ var SmallerFigure4 = document.getElementById('smallerfigure4');
 
 function ZoonIn(){
 RedIMG.src = "assets/Grupo novos graficos.svg";
-this.appendChild(RedIMG);
 RedIMG.style.position ='absolute';
 RedIMG.style.right = '0';
-RedIMG.style.top = '0';
+RedIMG.style.top = '-5%';
 RedIMG.style.width = '100%';
 RedIMG.style.transform = 'scale(1.1)';
 RedIMG.style.zIndex = '10';
+this.appendChild(RedIMG);
 };
 
 function ZoonOut(){
     RedIMG.remove();
-}
+};
+
 PrincipalFigure.onmouseover = SmallerFigure1.onmouseover = SmallerFigure2.onmouseover = SmallerFigure3.onmouseover = SmallerFigure4.onmouseover = ZoonIn;
 PrincipalFigure.onmouseleave = SmallerFigure1.onmouseleave = SmallerFigure2.onmouseleave = SmallerFigure3.onmouseleave = SmallerFigure4.onmouseleave = ZoonOut;
 
